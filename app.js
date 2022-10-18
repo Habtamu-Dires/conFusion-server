38b4735f0a2a4b113f23805bc94fce1c21ad2ba3
Habtamu-Dires <habtamuadugna.d@gmail.com>
2022-10-18 14:24:02 +0300

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,8 +14,6 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
-
-//const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -30,12 +31,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321')); //secret key
+//app.use(cookieParser('12345-67890-09876-54321')); //secret key
+app.use(session({
+  name:'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));  //this will add req.session in the request 
 
 function auth (req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if(!req.signedCookies.user){
+  if(!req.session.user){
       var authHeader = req.headers.authorization;
 
       if (!authHeader) {
@@ -51,9 +59,9 @@ function auth (req, res, next) {
       var user = auth[0];
       var pass = auth[1];
       if (user == 'admin' && pass == 'password') {
-          //let setup the cookie here
-          res.cookie('user', 'admin', {signed: true});  //this will promot the client to setup cookie
-          next(); // authorized                         // send the cookie in subsequent request.
+          //let setup the session here
+          req.session.user = 'admin';
+          next(); // authorized                      
       } else {
           var err = new Error('You are not authenticated!');
           res.setHeader('WWW-Authenticate', 'Basic');      
@@ -61,7 +69,7 @@ function auth (req, res, next) {
           next(err);
       }
   } else { //singned cookie already exist
-      if(req.signedCookies.user === 'admin') {
+      if(req.session.user === 'admin') {
         next();   // pass
       } else {
         var err = new Error('You are not authenticated!');
