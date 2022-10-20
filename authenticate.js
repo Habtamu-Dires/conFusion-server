35008486386d9c05d91519ee502f3f491fc35d72
Habtamu-Dires <habtamuadugna.d@gmail.com>
2022-10-20 12:11:27 +0300

    Mongoose Population

--- authenticate.js
+++ authenticate.js
@@ -30,7 +30,7 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         console.log("JWT payload: ", jwt_payload);
         User.findOne({_id: jwt_payload._id}, (err, user)=>{
             if (err) {
-                return done(err, false); // passport will pass to ur strategy.
+                return done(err, false); // passport will pass to ur strategy. req.user
             }
             else if(user) {
                 return done(null, user); 
