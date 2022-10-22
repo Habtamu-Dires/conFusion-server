df760eddda98341ab93933e1363b6110e0ff8266
Habtamu-Dires <habtamuadugna.d@gmail.com>
2022-10-22 12:47:38 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '502531835088773',
+        clientSecret: 'b31f6fe9b1804cc3f0364e01984888d3'
+    }
 }
